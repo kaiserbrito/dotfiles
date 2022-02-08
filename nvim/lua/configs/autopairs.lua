62b90c3125fda60b84d9d3606b9ddee337089e5b
Victor Guerra <victorbcguerra@gmail.com>
2022-02-08 12:39:10 +0000

    Added rules for autopairs

--- nvim/lua/configs/autopairs.lua
+++ nvim/lua/configs/autopairs.lua
@@ -27,6 +27,10 @@ function M.config()
     },
   }
 
+  npairs.add_rules(require('nvim-autopairs.rules.endwise-elixir'))
+  npairs.add_rules(require('nvim-autopairs.rules.endwise-lua'))
+  npairs.add_rules(require('nvim-autopairs.rules.endwise-ruby'))
+
   local cmp_autopairs = require "nvim-autopairs.completion.cmp"
   local cmp_status_ok, cmp = pcall(require, "cmp")
   if not cmp_status_ok then
