639e85c698ed1e971158a9da78e197e6740c3473
Victor Guerra <victor.guerra@getlavanda.com>
2022-02-18 11:39:48 +0000

    Added mappings for Gitsigns plugin

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -16,6 +16,24 @@ map("n", "<C-j>", "<C-w>j", opts)
 map("n", "<C-k>", "<C-w>k", opts)
 map("n", "<C-l>", "<C-w>l", opts)
 
+-- Gitsigns
+map("n", "<leader>j", "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", {expr=true})
+map("n", "<leader>k", "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", {expr=true})
+-- Actions
+map("n", "<leader>hs", ":Gitsigns stage_hunk<CR>", opts)
+map("v", "<leader>hs", ":Gitsigns stage_hunk<CR>", opts)
+map("n", "<leader>hr", ":Gitsigns reset_hunk<CR>", opts)
+map("v", "<leader>hr", ":Gitsigns reset_hunk<CR>", opts)
+map("n", "<leader>hS", "<cmd>Gitsigns stage_buffer<CR>", opts)
+map("n", "<leader>hu", "<cmd>Gitsigns undo_stage_hunk<CR>", opts)
+map("n", "<leader>hR", "<cmd>Gitsigns reset_buffer<CR>", opts)
+map("n", "<leader>hp", "<cmd>Gitsigns preview_hunk<CR>", opts)
+map("n", "<leader>hb", '<cmd>lua require"gitsigns".blame_line{full=true}<CR>', opts)
+map("n", "<leader>tb", "<cmd>Gitsigns toggle_current_line_blame<CR>", opts)
+map("n", "<leader>hd", "<cmd>Gitsigns diffthis<CR>", opts)
+map("n", "<leader>hD", '<cmd>lua require"gitsigns".diffthis("~")<CR>', opts)
+map("n", "<leader>td", "<cmd>Gitsigns toggle_deleted<CR>", opts)
+
 -- Clear search highlight
 map("n", "<leader>h", "<cmd>nohlsearch<CR>", opts)
 
