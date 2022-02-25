ddd43e98b78345bf80f1e8630f9becdac735da66
Victor Guerra <victor.guerra@getlavanda.com>
2022-02-25 09:50:21 +0000

    Disable spring

--- fish/config.fish
+++ fish/config.fish
@@ -11,30 +11,32 @@ alias ber="bundle exec rspec"
 alias be="bundle exec"
 
 set -x BUNDLE_GITHUB__COM x-access-token:$github_token
-set -x EDITOR "nvim"
+set -x EDITOR nvim
+set -x DISABLE_SPRING 1
 
 # Functions needed for !! and !$
 function __history_previous_command
     switch (commandline -t)
-    case "!"
-        commandline -t $history[1]; commandline -f repaint
-    case "*"
-        commandline -i !
+        case "!"
+            commandline -t $history[1]
+            commandline -f repaint
+        case "*"
+            commandline -i !
     end
 end
 
 function __history_previous_command_arguments
     switch (commandline -t)
-    case "!"
-        commandline -t ""
-        commandline -f history-token-search-backward
-    case "*"
-        commandline -i '$'
+        case "!"
+            commandline -t ""
+            commandline -f history-token-search-backward
+        case "*"
+            commandline -i '$'
     end
 end
 
 # The bindings for !! and !$
-if [ $fish_key_bindings = fish_vi_key_bindings ];
+if [ $fish_key_bindings = fish_vi_key_bindings ]
     bind -Minsert ! __history_previous_command
     bind -Minsert '$' __history_previous_command_arguments
 else
