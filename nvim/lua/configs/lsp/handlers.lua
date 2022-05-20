c522939585de0d052837bcfbfa55c893338e7252
Victor Guerra <victor.guerra@getlavanda.com>
2022-05-20 14:38:46 +0100

    Replace deprecated method

--- nvim/lua/configs/lsp/handlers.lua
+++ nvim/lua/configs/lsp/handlers.lua
@@ -54,7 +54,7 @@ local function lsp_keymaps(bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-p>", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
   -- Format command
-  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.format { async = true }' ]]
 end
 
 M.on_attach = function(client, bufnr)
