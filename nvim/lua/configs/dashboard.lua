d736248f243a734cbd728adfd235185e8c6208ad
Victor Guerra <victorbcguerra@gmail.com>
2022-06-24 12:24:36 +0100

    Refactor dashboard config

--- nvim/lua/configs/dashboard.lua
+++ nvim/lua/configs/dashboard.lua
@@ -1,27 +1,65 @@
 local M = {}
 
 function M.config()
+  local db = require('dashboard')
+  db.custom_header = {
+    '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ',
+    '⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡖⠁⠀⠀⠀⠀⠀⠀⠈⢲⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀ ',
+    '⠀⠀⠀⠀⠀⠀⠀⠀⣼⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣧⠀⠀⠀⠀⠀⠀⠀⠀ ',
+    '⠀⠀⠀⠀⠀⠀⠀⣸⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣇⠀⠀⠀⠀⠀⠀⠀ ',
+    '⠀⠀⠀⠀⠀⠀⠀⣿⣿⡇⠀⢀⣀⣤⣤⣤⣤⣀⡀⠀⢸⣿⣿⠀⠀⠀⠀⠀⠀⠀ ',
+    '⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣔⢿⡿⠟⠛⠛⠻⢿⡿⣢⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀ ',
+    '⠀⠀⠀⠀⣀⣤⣶⣾⣿⣿⣿⣷⣤⣀⡀⢀⣀⣤⣾⣿⣿⣿⣷⣶⣤⡀⠀⠀⠀⠀ ',
+    '⠀⠀⢠⣾⣿⡿⠿⠿⠿⣿⣿⣿⣿⡿⠏⠻⢿⣿⣿⣿⣿⠿⠿⠿⢿⣿⣷⡀⠀⠀ ',
+    '⠀⢠⡿⠋⠁⠀⠀⢸⣿⡇⠉⠻⣿⠇⠀⠀⠸⣿⡿⠋⢰⣿⡇⠀⠀⠈⠙⢿⡄⠀ ',
+    '⠀⡿⠁⠀⠀⠀⠀⠘⣿⣷⡀⠀⠰⣿⣶⣶⣿⡎⠀⢀⣾⣿⠇⠀⠀⠀⠀⠈⢿⠀ ',
+    '⠀⡇⠀⠀⠀⠀⠀⠀⠹⣿⣷⣄⠀⣿⣿⣿⣿⠀⣠⣾⣿⠏⠀⠀⠀⠀⠀⠀⢸⠀ ',
+    '⠀⠁⠀⠀⠀⠀⠀⠀⠀⠈⠻⢿⢇⣿⣿⣿⣿⡸⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠈⠀ ',
+    '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ',
+    '⠀⠀⠀⠐⢤⣀⣀⢀⣀⣠⣴⣿⣿⠿⠋⠙⠿⣿⣿⣦⣄⣀⠀⠀⣀⡠⠂⠀⠀⠀ ',
+    '⠀⠀⠀⠀⠀⠈⠉⠛⠛⠛⠛⠉⠀⠀⠀⠀⠀⠈⠉⠛⠛⠛⠛⠋⠁⠀⠀⠀⠀⠀ ',
+    '                               ',
+  }
+
+  db.preview_file_height = 12
+  db.preview_file_width = 80
+  db.custom_center = {
+    { icon = '  ',
+      desc = 'Recently opened files                 ',
+      action = 'Telescope oldfiles',
+      shortcut = 'SPC f h' },
+    { icon = '  ',
+      desc = 'Find  File                            ',
+      action = 'Telescope find_files find_command=rg,--hidden,--files',
+      shortcut = 'SPC f f' },
+    { icon = '  ',
+      desc = 'File Browser                          ',
+      action = 'NvimTreeToggle',
+      shortcut = ' SPC e ' },
+    { icon = '  ',
+      desc = 'Find  word                            ',
+      action = 'Telescope live_grep',
+      shortcut = 'SPC f g' },
+    { icon = '洛 ',
+      desc = 'New File                              ',
+      action = 'DashboardNewFile',
+      shortcut = 'SPC f n' },
+    { icon = '  ',
+      desc = 'Last Session                         ',
+      action = 'SessionLoad',
+      shortcut = 'SPC s l' },
+  }
   local g = vim.g
   local fn = vim.fn
 
   local plugins_count = fn.len(fn.globpath("~/.local/share/nvim/site/pack/packer/start", "*", 0, 1))
 
-  g.dashboard_disable_statusline = 1
-  g.dashboard_default_executive = "telescope"
-
-  g.dashboard_custom_section = {
-    a = { description = { "   Find File                 SPC f f" }, command = "Telescope find_files" },
-    b = { description = { "   Recents                   SPC f o" }, command = "Telescope oldfiles" },
-    c = { description = { "   Find Word                 SPC f w" }, command = "Telescope live_grep" },
-    d = { description = { " 洛 New File                  SPC f n" }, command = "DashboardNewFile" },
-    e = { description = { "   Bookmarks                 SPC b m" }, command = "Telescope marks" },
-    f = { description = { "   Last Session              SPC s l" }, command = "SessionLoad" },
-  }
-
   g.dashboard_custom_footer = {
     " ",
     " Neovim loaded " .. plugins_count .. " plugins ",
   }
+
+  g.indentLine_fileTypeExclude = { "dashboard" }
 end
 
 return M
