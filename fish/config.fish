f1a4d8475b4308ae269579e4a9bc206b76ea970d
Victor Guerra <victorbcguerra@gmail.com>
2022-11-06 22:12:08 +0000

    Add config var

--- fish/config.fish
+++ fish/config.fish
@@ -16,6 +16,7 @@ alias ber="bundle exec rspec"
 alias be="bundle exec"
 
 set -x BUNDLE_GITHUB__COM x-access-token:$github_token
+set -x BUNDLE_ENTERPRISE__CONTRIBSYS__COM $sidekiq_token
 set -x NPM_TOKEN $npm_token
 set -x EDITOR nvim
 set -x DISABLE_SPRING 1
