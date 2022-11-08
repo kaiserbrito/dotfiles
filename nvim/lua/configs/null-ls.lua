37c6a06bceac55cda8477dacd99b724b2249c2de
Victor Guerra <victorbcguerra@gmail.com>
2022-11-08 18:17:30 +0000

    Use project rubocop version

--- nvim/lua/configs/null-ls.lua
+++ nvim/lua/configs/null-ls.lua
@@ -16,6 +16,11 @@ function M.config()
   -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
   local diagnostics = null_ls.builtins.diagnostics
 
+  local conditional = function(fn)
+    local utils = require("null-ls.utils").make_conditional_utils()
+    return fn(utils)
+  end
+
   null_ls.setup {
     debug = false,
     sources = {
@@ -23,7 +28,7 @@ function M.config()
       formatting.mix,
       -- JS
       formatting.prettier.with {
-        extra_args = {"--single-quote"}
+        extra_args = { "--single-quote" }
       },
       -- Fish
       formatting.fish_indent,
@@ -32,8 +37,22 @@ function M.config()
       formatting.gofmt,
       formatting.goimports,
       -- Ruby
-      formatting.rubocop,
-      diagnostics.rubocop,
+      conditional(function(utils)
+        return utils.root_has_file("Gemfile")
+            and null_ls.builtins.formatting.rubocop.with({
+              command = "bundle",
+              args = vim.list_extend({ "exec", "rubocop" }, null_ls.builtins.formatting.rubocop._opts.args),
+            })
+            or null_ls.builtins.formatting.rubocop
+      end),
+      conditional(function(utils)
+        return utils.root_has_file("Gemfile")
+            and null_ls.builtins.diagnostics.rubocop.with({
+              command = "bundle",
+              args = vim.list_extend({ "exec", "rubocop" }, null_ls.builtins.diagnostics.rubocop._opts.args),
+            })
+            or null_ls.builtins.diagnostics.rubocop
+      end),
       -- Terraform
       formatting.terraform_fmt,
     },
