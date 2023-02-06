13b0a629f0ef9059e68ef76e88d1b99cdfb43bde
Victor Guerra <victorbcguerra@gmail.com>
2023-02-06 12:53:58 +0000

    Improve bufferline config

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -53,7 +53,7 @@ return packer.startup(function(use)
 
   use("kyazdani42/nvim-web-devicons")
 
-  use({ "akinsho/bufferline.nvim", requires = "kyazdani42/nvim-web-devicons" }) -- Buffer
+  use({ "akinsho/bufferline.nvim" })
 
   use("moll/vim-bbye") -- Delete buffers
 
