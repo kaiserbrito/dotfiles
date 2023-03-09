22c4178466c736507d5a28dfe12e59e1c07fd6d7
Victor Guerra <victorbcguerra@gmail.com>
2023-03-09 10:12:49 +0000

    Replace nvim-tree config

--- nvim/lua/configs/nvim-tree.lua
+++ nvim/lua/configs/nvim-tree.lua
@@ -4,111 +4,17 @@ if not setup then
   return
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-  return
-end
-
 -- recommended settings from nvim-tree documentation
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- change color for arrows in tree to light blue
-vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
-
-local tree_cb = nvim_tree_config.nvim_tree_callback
 -- configure nvim-tree
 nvimtree.setup({
-  auto_reload_on_write = true,
+  sort_by = "case_sensitive",
+  renderer = {
+    group_empty = true,
+  },
   filters = {
-    dotfiles = false,
-    custom = {
-      "^.git$",
-      "node_modules",
-      ".cache",
-    },
+    dotfiles = true,
   },
-  disable_netrw = false,
-  hijack_netrw = true,
-  open_on_tab = false,
-  hijack_cursor = true,
-  hijack_unnamed_buffer_when_opening = false,
-  hijack_directories = {
-    enable = true,
-    auto_open = true,
-  },
-  update_cwd = true,
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
-    ignore_list = {},
-  },
-  system_open = {
-    cmd = "",
-    args = {},
-  },
-  diagnostics = {
-    enable = false,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    },
-  },
-  view = {
-    hide_root_folder = false,
-    mappings = {
-      custom_only = false,
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-      },
-    },
-    number = false,
-    preserve_window_proportions = false,
-    relativenumber = false,
-    side = "left",
-    signcolumn = "yes",
-    width = 30,
-  },
-  git = {
-    enable = true,
-    ignore = false,
-    timeout = 500,
-  },
-  actions = {
-    use_system_clipboard = true,
-    change_dir = {
-      enable = true,
-      global = false,
-      restrict_above_cwd = false,
-    },
-    open_file = {
-      quit_on_open = false,
-      resize_window = false,
-      window_picker = {
-        enable = true,
-        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-        exclude = {
-          filetype = { "notify", "qf", "diff", "fugitive", "fugitiveblame" },
-          buftype = { "nofile", "terminal", "help" },
-        },
-      },
-    },
-  },
-  log = {
-    enable = false,
-    truncate = false,
-    types = {
-      all = false,
-      config = false,
-      copy_paste = false,
-      diagnostics = false,
-      git = false,
-      profile = false,
-    },
-  },
-  -- change folder arrow icons
 })
