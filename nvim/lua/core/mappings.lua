812b682f69e45a1aed357035044ae853157b1287
Victor Guerra <victorbcguerra@gmail.com>
2023-05-17 11:51:49 +0100

    Remove esc mapping for term

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -117,7 +117,6 @@ map("v", "<leader>/", "<esc><cmd>lua require('Comment.api').toggle_linewise_op(v
 
 -- Terminal
 map("n", "<C-\\>", '<cmd>ToggleTerm<CR>', opts)
-map("t", "<esc>", [[<C-\><C-n>]], opts)
 map('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
 map('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
 map('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
