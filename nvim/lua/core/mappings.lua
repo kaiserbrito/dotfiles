7c2ed22ed6f8efcae87f0a1e565b5276fad45a35
Victor Guerra <victorbcguerra@gmail.com>
2023-08-10 10:39:17 +0100

    Remove nvim-dap config

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -82,17 +82,6 @@ map("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", opts)
 map("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>", opts)
 map("n", "<leader>fr", "<cmd>Telescope registers<CR>", opts)
 
--- Debugger
-map("n", "<F5>", "<cmd>lua require'dap'.continue()<CR>", opts)
-map("n", "<F10>", "<cmd>lua require'dap'.step_over()<CR>", opts)
-map("n", "<F11>", "<cmd>lua require'dap'.step_into()<CR>", opts)
-map("n", "<F12>", "<cmd>lua require'dap'.step_out()<CR>", opts)
-map("n", "<leader>b", "<cmd>lua require'dap'.toggle_breakpoint()<CR>", opts)
-map("n", "<leader>B", "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", opts)
-map("n", "<leader>lp", "<cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>", opts)
-map("n", "<leader>dr", "<cmd>lua require'dap'.repl.open()<CR>", opts)
-map("n", "<leader>dl", "<cmd>lua require'dap'.run_last()<CR>", opts)
-
 -- Trouble
 map("n", "<leader>xx", "<cmd>TroubleToggle<CR>", opts)
 map("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<CR>", opts)
