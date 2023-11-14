d8a8fe5c2e3d64745035ae9483647045902660a4
Victor Guerra <victorbcguerra@gmail.com>
2023-11-14 15:05:01 +0000

    Improve indent-blankline config

--- nvim/lua/configs/indent-blankline.lua
+++ nvim/lua/configs/indent-blankline.lua
@@ -1,4 +1,12 @@
-vim.opt.list = true
-vim.opt.listchars:append "eol:↴"
-
-require("ibl").setup {}
+local highlight = {
+  "CursorColumn",
+  "Whitespace",
+}
+require("ibl").setup {
+  indent = { highlight = highlight, char = "" },
+  whitespace = {
+    highlight = highlight,
+    remove_blankline_trail = false,
+  },
+  scope = { enabled = false },
+}
