bc9cb5aac5264c9d9a579b6486019330c53b7d57
Victor Guerra <victorbcguerra@gmail.com>
2023-11-24 15:29:35 +0000

    Add mapping

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -149,3 +149,5 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+vim.cmd[[iabbr bp binding.pry]]
