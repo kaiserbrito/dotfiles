b34e0f9a4c35dc16021bb20ce601582ba4814d63
Victor Guerra <victorbcguerra@gmail.com>
2024-01-04 10:45:41 +0000

    Change vim-test strategy

--- nvim/lua/configs/vim-test.lua
+++ nvim/lua/configs/vim-test.lua
@@ -1,3 +1,5 @@
 vim.api.nvim_set_keymap('n', '<leader>tn', '<cmd>TestNearest<CR>', { noremap = true, silent = true}) -- Test nearest test
 vim.api.nvim_set_keymap('n', '<leader>tf', '<cmd>TestFile<CR>', { noremap = true, silent = true}) -- Test file
 vim.api.nvim_set_keymap('n', '<leader>tl', '<cmd>TestLast<CR>', { noremap = true, silent = true}) -- Test last test run
+
+vim.cmd([[ let test#strategy = "neovim_sticky" ]])
