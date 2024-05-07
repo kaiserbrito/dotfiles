6f7dffb642d389324319cd306093ba0bbe6c658e
Victor Guerra <victor.guerra@amenitiz.com>
2024-05-07 09:36:02 +0100

    Change keymaps

--- nvim/lua/configs/copilot.lua
+++ nvim/lua/configs/copilot.lua
@@ -6,7 +6,7 @@ vim.g.copilot_filetypes = { xml = false }
 -- vim.g.copilot_filetypes = { ["*"] = false, python = true }
 
 
-vim.cmd[[imap <silent><script><expr> <M-l> copilot#Accept("\<CR>")]]
+vim.cmd[[imap <silent><script><expr> <C-a> copilot#Accept("\<CR>")]]
 vim.g.copilot_no_tab_map = true
 -- vim.keymap.set.keymap("i", "<C-a>", ":copilot#Accept('\\<CR>')<CR>", { silent = true })
 
