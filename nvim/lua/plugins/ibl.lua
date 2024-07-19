a82767b4ac63c1bec4f36894cd64c2542849fede
Victor Guerra <victor.guerra@amenitiz.com>
2024-07-19 08:32:35 +0100

    Use mini instead

--- nvim/lua/plugins/ibl.lua
+++ nvim/lua/plugins/ibl.lua
@@ -1,19 +1,7 @@
 return {
-  "lukas-reineke/indent-blankline.nvim",
-  main = "ibl",
-  event = "VeryLazy",
+  "echasnovski/mini.indentscope",
+  version = "*",
   config = function ()
-    local highlight = {
-      "CursorColumn",
-      "Whitespace",
-    }
-    require("ibl").setup {
-      indent = { highlight = highlight, char = "" },
-      whitespace = {
-        highlight = highlight,
-        remove_blankline_trail = false,
-      },
-      scope = { enabled = false },
-    }
-  end,
+    require("mini.indentscope").setup()
+  end
 }
