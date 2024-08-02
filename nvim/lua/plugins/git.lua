c343c07b960af2b3bd47e9806a4cd2417ca2780d
Victor Guerra <victor.guerra@amenitiz.com>
2024-08-02 15:54:25 +0100

    Replace telescope for fzf-lua instead

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -74,7 +74,6 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "sindrets/diffview.nvim",
-      "nvim-telescope/telescope.nvim",
     },
     config = function ()
       require("neogit").setup()
