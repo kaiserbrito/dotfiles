004d0decf71d63f0c5db0e63b794c01c1897e047
Victor Guerra <victor.guerra@amenitiz.com>
2024-09-07 11:30:03 +0100

    Improve lsp config
    Use ruby_lsp instead of solargraph

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -20,31 +20,34 @@ return {
       "hrsh7th/cmp-nvim-lsp",
     },
     config = function()
+      local lspconfig = require("lspconfig")
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-      local lsp_attach = function(client, bufnr)
-        local opts = { noremap = true, silent = true, buffer = bufnr }
-        local keymap = vim.keymap
-        keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-        keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-        keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
-        keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-        keymap.set("n", "gr", vim.lsp.buf.references, opts)
-        keymap.set("n", "<leader>f", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", opts)
-      end
+      lspconfig.html.setup {
+        capabilities = capabilities,
+      }
+      lspconfig.lua_ls.setup {
+        capabilities = capabilities,
+      }
+      lspconfig.ruby_lsp.setup {
+        capabilities = capabilities,
+      }
+      lspconfig.tailwindcss.setup {
+        capabilities = capabilities,
+      }
+      lspconfig.tsserver.setup {
+        capabilities = capabilities,
+      }
 
-      local lspconfig = require("lspconfig")
-      local get_servers = require("mason-lspconfig").get_installed_servers
-
-      for _, server_name in ipairs(get_servers()) do
-        lspconfig[server_name].setup {
-          on_attach = lsp_attach,
-          capabilities = capabilities,
-        }
-      end
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+      vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>f", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", {})
     end,
   },
 }
