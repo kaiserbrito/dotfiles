731a7a5332c50ba22959935719fd637ac4551c21
Victor Guerra <victor.guerra@amenitiz.com>
2024-09-17 10:40:31 +0100

    Add formatter for erb file

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -16,6 +16,7 @@ return {
     formatters_by_ft = {
       javascript = { { "prettierd", "prettier", stop_after_first = true } },
       ruby = { "rubocop" },
+      eruby = { "erb_format" },
     },
   },
 }
