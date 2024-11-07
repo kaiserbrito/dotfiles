5c23bb0d8af066f1b59db89179544956894a4049
Victor Guerra <victorbcguerra@gmail.com>
2024-11-07 21:07:31 +0100

    Replace nvim-cmp for blink.cmp (#27)
    Use blink-cmp instead of nvim-cmp

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -26,11 +26,11 @@ return {
     "neovim/nvim-lspconfig",
     lazy = false,
     dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
+      "saghen/blink.cmp",
     },
     config = function()
       local lspconfig = require("lspconfig")
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
       local get_servers = require("mason-lspconfig").get_installed_servers
 
       for _, server_name in ipairs(get_servers()) do
