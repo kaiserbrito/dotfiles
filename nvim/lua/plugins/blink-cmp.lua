d1f9d2852911072d3943cfea0d3488c9a4989dac
Victor Guerra <victor.guerra@amenitiz.com>
2024-12-19 08:33:56 +0000

    Improve blink.cmp config

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -1,17 +1,17 @@
 return {
   "saghen/blink.cmp",
-  lazy = false,
   dependencies = "rafamadriz/friendly-snippets",
   version = "v0.*",
   opts = {
     keymap = { preset = "default" },
-    highlight = {
+    appearance = {
       use_nvim_cmp_as_default = true,
+      nerd_font_variant = "mono"
     },
-    nerd_font_variant = 'mono',
-    -- experimental auto-brackets support
-    accept = { auto_brackets = { enabled = true } },
-    -- experimental signature help support
-    trigger = { signature_help = { enabled = true } }
-  }
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+    },
+    signature = { enabled = true }
+  },
+  opts_extend = { "sources.default" }
 }
