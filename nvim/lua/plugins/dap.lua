535d2b0c3c7e213b34064e20b65ca146f8e8457a
Victor Guerra <victor.guerra@amenitiz.com>
2025-01-23 08:53:03 +0000

    Install nvim-dap

---
+++ nvim/lua/plugins/dap.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+    },
+    config = function()
+      require("dapui").setup()
+
+      vim.fn.sign_define(
+        "DapBreakpoint", {
+          text = "🔴",
+          texthl = "DapBreakpoint",
+          linehl = "DapBreakpoint",
+          numhl = "DapBreakpoint",
+        }
+      )
+      vim.api.nvim_set_keymap("n", "<leader>dt", ":DapUiToggle<CR>", { noremap = true })
+      vim.api.nvim_set_keymap("n", "<leader>db", ":DapToggleBreakpoint<CR>", { noremap = true })
+      vim.api.nvim_set_keymap("n", "<leader>dc", ":DapContinue<CR>", { noremap = true })
+      vim.api.nvim_set_keymap("n", "<leader>dr", ":lua require('dapui').open({reset = true})<CR>", { noremap = true })
+    end,
+  },
+
+  {
+    "leoluz/nvim-dap-go",
+    config = function()
+      require("dap-go").setup()
+    end,
+  },
+}
