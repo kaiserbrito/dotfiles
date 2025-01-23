2da7cd0e08b2375c9ec9b6bb357cf660b841bd4c
Victor Guerra <victor.guerra@amenitiz.com>
2025-01-23 08:52:30 +0000

    Use gruvbox-material for nvim colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,29 +1,12 @@
 return {
-  "rose-pine/neovim",
-  name = "rose-pine",
+  "sainnhe/gruvbox-material",
   lazy = false,
   priority = 1000,
-  opts = {},
   config = function()
-    require("rose-pine").setup({
-      variant = "moon",    -- auto, main, moon, or dawn
-      dark_variant = "main", -- main, moon, or dawn
-      dim_inactive_windows = true,
-      extend_background_behind_borders = true,
-
-      enable = {
-        terminal = true,
-        legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-        migrations = true,        -- Handle deprecated options automatically
-      },
-
-      styles = {
-        bold = true,
-        italic = true,
-        transparency = true,
-      },
-    })
-
-    vim.cmd.colorscheme("rose-pine-moon")
-  end,
+    vim.g.gruvbox_material_enable_italic = true
+    vim.g.gruvbox_material_transparent_background = 2
+    vim.g.gruvbox_material_dim_inactive_windows = 1
+    vim.g.gruvbox_material_float_style = "dim"
+    vim.cmd.colorscheme("gruvbox-material")
+  end
 }
