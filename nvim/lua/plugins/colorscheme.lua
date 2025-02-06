9928ba7d32beb8c87d26d657af92b10f985f21d5
Victor Guerra <victor.guerra@amenitiz.com>
2025-02-06 12:30:59 +0000

    Change colorscheme to catppuccin

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,18 @@
 return {
-  "sainnhe/gruvbox-material",
+  "catppuccin/nvim",
+  name = "catppuccin",
   lazy = false,
   priority = 1000,
   config = function()
-    vim.g.gruvbox_material_enable_italic = true
-    vim.g.gruvbox_material_transparent_background = 2
-    vim.g.gruvbox_material_dim_inactive_windows = 1
-    vim.g.gruvbox_material_float_style = "dim"
-    vim.cmd.colorscheme("gruvbox-material")
+    require("catppuccin").setup({
+      transparent_background = true,
+      dim_inactive = {
+        enabled = true,
+        shade = "dark",
+        percentage = 0.15,
+      },
+    })
+
+    vim.cmd("colorscheme catppuccin-macchiato")
   end
 }
