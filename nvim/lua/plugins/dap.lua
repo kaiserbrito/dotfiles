bd48911adcf02a6178704e19e1fe8c10071d7fcf
Victor Guerra <victor.guerra@amenitiz.com>
2025-02-13 08:32:08 +0000

    Add mapping to clear breakpoints

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -20,6 +20,7 @@ return {
       vim.api.nvim_set_keymap("n", "<leader>db", ":DapToggleBreakpoint<CR>", { noremap = true })
       vim.api.nvim_set_keymap("n", "<leader>dc", ":DapContinue<CR>", { noremap = true })
       vim.api.nvim_set_keymap("n", "<leader>dr", ":lua require('dapui').open({reset = true})<CR>", { noremap = true })
+      vim.api.nvim_set_keymap("n", "<leader>dbc", ":DapClearBreakpoints<CR>", { noremap = true })
     end,
   },
 
