a533ed489922a21591e686f35ddce48fac381e73
Victor Guerra <victor.guerra@amenitiz.com>
2026-03-19 08:19:26 +0000

    improve fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    mise activate fish | source
 end
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
@@ -17,7 +18,6 @@ abbr -a lt eza --tree --level=2 --long --icons --git
 abbr -a ltree eza --tree --level=2  --icons --git
 
 
-fish_add_path /opt/homebrew/opt/postgresql@17/bin
 # Exports
 set -x EDITOR nvim
 set -x DISABLE_SPRING 1
@@ -25,12 +25,13 @@ set -x TERM screen-256color
 set -x FZF_DEFAULT_COMMAND "fd"
 set -x GPG_TTY (tty)
 set -x RUBY_YJIT_ENABLE 1
-set -gx LDFLAGS "-L/opt/homebrew/opt/postgresql@17/lib"
-set -gx CPPFLAGS "-I/opt/homebrew/opt/postgresql@17/include"
 set -x FPROF 1
+set -l output (mole completion fish 2>/dev/null); and echo "$output" | source
+
+# Manual override if activation hook fails to prepend
+fish_add_path --move --prepend "$HOME/.local/share/mise/shims"
 
 direnv hook fish | source
 fzf --fish | source
 zoxide init fish | source
 starship init fish | source
-mise activate fish | source
