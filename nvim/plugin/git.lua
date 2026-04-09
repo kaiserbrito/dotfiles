e0436715193e106b17db73d0bf90243e4a6ad88e
Victor Guerra <victorbcguerra@gmail.com>
2026-04-09 07:46:28 +0100

    fix gitsigns keymaps

--- nvim/plugin/git.lua
+++ nvim/plugin/git.lua
@@ -43,23 +43,69 @@ require("gitsigns").setup({
     row = 0,
     col = 1,
   },
-})
+  on_attach = function (bufnr)
+    local gitsigns = require("gitsigns")
+        local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
 
-vim.keymap.set("n", "<leader>j", "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true })
-vim.keymap.set("n", "<leader>k", "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true })
-vim.keymap.set("n", "<leader>hs", ":Gitsigns stage_hunk<CR>")
-vim.keymap.set("v", "<leader>hs", ":Gitsigns stage_hunk<CR>")
-vim.keymap.set("n", "<leader>hr", ":Gitsigns reset_hunk<CR>")
-vim.keymap.set("v", "<leader>hr", ":Gitsigns reset_hunk<CR>")
-vim.keymap.set("n", "<leader>hu", ":Gitsigns undo_stage_hunk<CR>")
-vim.keymap.set("v", "<leader>hu", ":Gitsigns undo_stage_hunk<CR>")
-vim.keymap.set("n", "<leader>hS", ":Gitsigns stage_buffer<CR>")
-vim.keymap.set("n", "<leader>hR", ":Gitsigns reset_buffer<CR>")
-vim.keymap.set("n", "<leader>gb", ":Gitsigns blame<CR>")
-vim.keymap.set("n", "<leader>tb", ":Gitsigns toggle_current_line_blame<CR>")
-vim.keymap.set("n", "<leader>hd", ":Gitsigns diffthis<CR>")
-vim.keymap.set("n", "<leader>td", ":Gitsigns toggle_deleted<CR>")
-vim.keymap.set("n", "<leader>hD", '<cmd>lua require"gitsigns".diffthis("~")<CR>')
+    -- Navigation
+    map('n', '<leader>j', function()
+      if vim.wo.diff then
+        vim.cmd.normal({']c', bang = true})
+      else
+        gitsigns.nav_hunk('next')
+      end
+    end)
+
+    map('n', '<leader>k', function()
+      if vim.wo.diff then
+        vim.cmd.normal({'[c', bang = true})
+      else
+        gitsigns.nav_hunk('prev')
+      end
+    end)
+
+    -- Actions
+    map('n', '<leader>hs', gitsigns.stage_hunk)
+    map('n', '<leader>hr', gitsigns.reset_hunk)
+
+    map('v', '<leader>hs', function()
+      gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+    end)
+
+    map('v', '<leader>hr', function()
+      gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+    end)
+
+    map('n', '<leader>hS', gitsigns.stage_buffer)
+    map('n', '<leader>hR', gitsigns.reset_buffer)
+    map('n', '<leader>hp', gitsigns.preview_hunk)
+    map('n', '<leader>hi', gitsigns.preview_hunk_inline)
+
+    map('n', '<leader>gb', function()
+      gitsigns.blame_line({ full = true })
+    end)
+
+    map('n', '<leader>hd', gitsigns.toggle_deleted)
+
+    map('n', '<leader>hD', function()
+      gitsigns.diffthis('~')
+    end)
+
+    map('n', '<leader>hQ', function() gitsigns.setqflist('all') end)
+    map('n', '<leader>hq', gitsigns.setqflist)
+
+    -- Toggles
+    map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+    map('n', '<leader>tw', gitsigns.toggle_word_diff)
+
+    -- Text object
+    map({'o', 'x'}, 'ih', gitsigns.select_hunk)
+  end
+})
 
 require("neogit").setup()
 
