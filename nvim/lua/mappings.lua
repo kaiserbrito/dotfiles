5d36755dc73c844f6336879830f23d66c946e47c
Victor Guerra <victorbcguerra@gmail.com>
2026-06-22 19:29:36 +0100

    keymap to move selected lines

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -46,5 +46,8 @@ vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
 vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Check references" })
 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "moves lines down in visual selection" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "moves lines up in visual selection" })
+
 vim.cmd [[iabbr bp binding.pry]]
 vim.cmd [[iabbr dbg require "debug"; debugger]]
